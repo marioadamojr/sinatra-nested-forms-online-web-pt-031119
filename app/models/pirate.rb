45143ed4844e42d6c55e00801d5bb24da13d67b9
Mario Adamo Jr <marioadamojr@gmail.com>
2019-06-17 01:23:03 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,12 @@
 class Pirate
+  attr_reader :name, :weight, :height
+
+  def initialize(params)
+    # hash_info.each do |k,v|
+    #   self.send(("#{k}="), v)
+    # end
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+  end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,23 @@
 class Ship
+  attr_reader :name, :type, :booty
+
+  @@ships = []
+
+  def initialize(args)
+    # hash_info.each do |k,v|
+    #   self.send(("#{k}="), v)
+    # end
+    @name=args[:name]
+    @type=args[:type]
+    @booty=args[:booty]
+    @@ships << self
+  end
+
+  def self.clear
+    @@ships.clear
+  end
+
+  def self.all
+    @@ships
+  end
 end
